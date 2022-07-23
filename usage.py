bbb68f9efb82db465749a7ebccbe7fde852e68f3
Koichiro <79580751+koichiro61@users.noreply.github.com>
2022-07-24 07:04:10 +0900

    Create usage.py

---
+++ usage.py
@@ -0,0 +1,15 @@
+from horeki import *
+
+hhh = ['明治34年3月2日', '昭和 36 年 4 月 21 日', '平成元年12月', '令和 2年']
+
+for ddd in hhh:
+	print(horeki2seireki(ddd))
+  
+'''
+result
+
+1901-03-02
+1961-04-21
+1990-12-01
+2020-01-01
+'''
